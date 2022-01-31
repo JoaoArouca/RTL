70d568a58d9f7a94708f21bcbab80be8f860e185
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-01-31 20:33:47 -0300

    req04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      const text = screen.getByRole('heading', {
+        name: /page/i,
+      });
+      expect(text).toBeInTheDocument();
+      expect(text).toHaveTextContent(/Page requested not found/i);
+    });
+
+  it('Teste se página mostra a imagem', () => {
+    const img = screen.getByRole('img', {
+      name: /pikachu/i,
+    });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
