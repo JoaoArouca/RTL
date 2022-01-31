62253c3f536c7178a50014ae9083b06844d806c5
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-01-31 19:20:46 -0300

    req01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('1 - Testa o componente App', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    expect(home).toBeInTheDocument();
+
+    const About = screen.getByRole('link', {
+      name: /About/i,
+    });
+    expect(About).toBeInTheDocument();
+
+    const favorites = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(favorites).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', {
+      name: /Home/i,
+    }));
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+    userEvent.click(about);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    /* poderia usar também o window.location  */
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favorite);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação éredirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('anything');
+    const found = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(found).toBeInTheDocument();
+  });
+});
